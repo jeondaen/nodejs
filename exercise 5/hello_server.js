3864a5dd61218df57509187537b9d00398ac584d
Jeon Daen <jeondaen@gmail.com>
2020-03-26 02:25:12 +0900

    exercise 5

--- exercise 5/hello_server.js
+++ exercise 5/hello_server.js
@@ -1,11 +1,9 @@
 const http = require('http');
 
 var server = http.createServer((req, res) => {
-    console.log('server created');
-});
-
-http.request({path: 'http://localhost:80', headers: {'Content-Type' : 'text/html;'}}, (res) => {
-    console.log('hello? request');
-})
-
-server.listen(80);
+   if(req.method === 'GET') {
+       if(req.url === '/') {
+           console.log('Hello world!');
+       }
+   }
+}).listen(80);
