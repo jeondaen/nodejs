3f25ae6484052b0f2417aa58fb33191085751339
jeondaen <jeondaen@gmail.com>
2020-03-29 19:17:52 +0900

    final version

--- exercise 6/notes_server.js
+++ exercise 6/notes_server.js
@@ -1,24 +1,25 @@
 const http = require('http');
 const fs = require('fs');
 
-let memo = ['Buy milk', 'Some eggs', 'Go to work', 'reactjsexample.com'];
+const memo = [];
 
 http.createServer((req, res) => {
     if(req.method === 'POST') {
         if(req.url === '/add') {
-            fs.writeFileSync('memo.txt', JSON.stringify(memo));
+            req.on('data', (data) => {
+                data = data.toString();
+
+                memo.push(data);
+            })
         }
     }
 
     if(req.method === 'GET') {
         if(req.url === '/list') {
-            fs.readFile('memo.txt', (err, data) => {
-                if(err) throw err;
 
                 res.setHeader('Content-Type', 'text/html;');
-                res.write(data);
+                res.write(memo.toString());
                 res.end();
-            })
         }
     }
 }).listen(80);
